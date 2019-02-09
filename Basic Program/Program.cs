0cd68fa2013df0bf28b09f6c54f61e256670fbc8
Dayghini <39215718+Dayghini@users.noreply.github.com>
2019-02-09 12:25:09 +0200

    Add files via upload

---
+++ Basic Program/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trying_My_Best_At_It
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = int.Parse(Console.ReadLine());
+            for (int i=0; i<c; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            int n = int.Parse(Console.ReadLine());
+            for (int d = 0; d < n; d--)
+            {
+                Console.WriteLine(d);
+            }
+        }
+    }
+}
