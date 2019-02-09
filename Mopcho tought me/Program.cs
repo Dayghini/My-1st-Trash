c510b9704768b8eda0b6cab6bb7ce4e7ea9778d7
Dayghini <39215718+Dayghini@users.noreply.github.com>
2019-02-09 12:23:08 +0200

    Add files via upload

---
+++ Mopcho tought me/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mopcho_tought_me
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = ("1000");
+            int b = (10);
+            b = int.Parse(a)+b;
+            Console.WriteLine(b);
+        }
+    }
+}
